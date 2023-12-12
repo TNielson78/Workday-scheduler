fc65e7af2308ad9ced9afa5bac8c916ad481912d
Travis Nielson <147010160+TNielson78@users.noreply.github.com>
2023-12-12 16:54:48 -0700

    Add files via upload
    Updated files with updated comments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var currentTime = dayjs()
 var currentHour = dayjs().hour()
 $(document).ready(function(){
+  //Updates current block based on past, present or future based on dayjs..
 for (var i = 9; i < 18; i++) {
   if (i < currentHour) {
     document.getElementById("hour-" + i).classList.add("past")
@@ -26,7 +27,7 @@ $(".time-block").each(function () {
 
   $(this).children(".description").val(localStorage.getItem(rowHour))
 })
-
+// Updates time and date format..
 setInterval(function(){
   var time=dayjs()
   $("#time").text(time.format("MMMM D, YYYY| hh:mm:ss"))
@@ -40,28 +41,5 @@ setInterval(function(){
 
 
 
-// get a new date (locale machine date time)
-// get the date as a string
-// get the time as a string
-// find the html element with the id of time
-// set the innerHTML of that element to the date a space the time
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
 
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO: Add code to display the current date in the header of the page.
 
